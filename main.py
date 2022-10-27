8f85f1ef2a3a8167e4353f48b56d936730857c76
Deadly <73450703+deadly@users.noreply.github.com>
2022-10-27 19:49:32 -0400

    Add files via upload
    - Added support for multiple agents

--- main.py
+++ main.py
@@ -1,36 +1,77 @@
 import json
-from requests import session
+import requests
 from valclient.client import Client
+
 print('Valorant Agent Yoinker by https://github.com/deadly')
-playerRegion = input('Enter your region (e.g NA): ').lower()
-client = Client(region=playerRegion)
-client.activate()
 valid = False
 agents = {}
 seenMatches = []
+choice = ''
+
+def findMapName(id):
+    r = requests.get(f"https://valorant-api.com/v1/maps/{id}")
+    data = r.json()
+    return data['data']['displayName']
 
 with open('data.json', 'r') as f:
-    agents = json.load(f)
+    data = json.load(f)
+    agents = data['agents']
+    maps = data['maps']
+    ranBefore = data['ran']
+    mapCodes = data['codes']
+    region = data['region']
 
-while valid == False:
-    try: 
-        preferredAgent = input("Preferred Agent (e.g Kayo): ").lower()
-        if (preferredAgent in agents['agents'].keys()):
-            valid = True
-        else:
-            print("Invalid Agent")
-    except:
-        print("Input Error")
 
-print("Waiting for Agent Select")
+if (ranBefore == True):
+        choice = input("type S to start or type C to change preferred agents: ").lower()
+
+if (ranBefore == False or choice == 'c'): 
+    playerRegion = input('Enter your region (e.g NA): ').lower()
+    client = Client(region=playerRegion)
+    client.activate()
+
+    
+    print("\nPlease Enter Preferred Agent For Each Map (e.g kayo)")
+    print("_________________________________________________________\n")
+
+    for map in maps.keys():
+        valid = False
+        while valid == False:
+            try:
+                preferredAgent = input(f"Preferred agent on {mapCodes[map].capitalize()}: ").lower()
+                if (preferredAgent in agents.keys()):
+                    maps[map] = agents[preferredAgent]
+                    valid = True
+                else:
+                    print("Invalid Agent")
+            except:
+                print("Input Error")
+    
+    with open('data.json', 'w') as f:
+            data['maps'] = maps
+            data['ran'] = True
+            data['region'] = playerRegion
+            json.dump(data, f)
+
+else:
+    client = Client(region=region)
+    client.activate()
+
+
+print("Waiting for Agent Select\n")
 while True:
     try:
         sessionState = client.fetch_presence(client.puuid)['sessionLoopState']
-        if ((sessionState == "PREGAME") and (client.pregame_fetch_match()['ID'] not in seenMatches)):
-            print('Agent Select Found')
-            client.pregame_select_character(agents['agents'][preferredAgent])
-            client.pregame_lock_character(agents['agents'][preferredAgent])
+        matchID = client.pregame_fetch_match()['ID']
+        if ((sessionState == "PREGAME") and (matchID not in seenMatches)):
+            matchInfo = client.pregame_fetch_match(matchID)
+            mapName = matchInfo["MapID"].split('/')[-1].lower()
+            print(f'Agent Select Found - {mapCodes[mapName].capitalize()}')
+
+            client.pregame_select_character(maps[mapName])
+            client.pregame_lock_character(maps[mapName])
             seenMatches.append(client.pregame_fetch_match()['ID'])
-            print('Successfully Locked ' + preferredAgent.capitalize())
+            print('Agent Locked - ' + list(agents.keys())[list(agents.values()).index(maps[mapName])].capitalize())
+
     except Exception as e:
         print('', end='') # Using pass caused weird behavior

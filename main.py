8e04ca0d1c4f5598d36ad6f2d14707373eda6740
Deadly <73450703+deadly@users.noreply.github.com>
2023-01-18 14:39:55 -0500

    Refactored code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-import requests
+import json, threading
 from valclient.client import Client
 
 print('Valorant Agent Yoinker by https://github.com/deadly')
@@ -8,10 +7,51 @@ agents = {}
 seenMatches = []
 choice = ''
 
-def findMapName(id):
-    r = requests.get(f"https://valorant-api.com/v1/maps/{id}")
-    data = r.json()
-    return data['data']['displayName']
+# set_interval Credit: https://stackoverflow.com/users/1909864/stamat
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+def lock_agent():
+    global seenMatches
+
+    in_menus = True
+
+    while in_menus:
+        try:
+            sessionState = client.fetch_presence(client.puuid)['sessionLoopState']
+            matchID = client.pregame_fetch_match()['ID']
+
+            if (sessionState == "PREGAME" and matchID not in seenMatches):
+                in_menus = False
+                seenMatches.append(matchID)
+                seenMatches.append(matchID)
+                matchInfo = client.pregame_fetch_match(matchID)
+                mapName = matchInfo["MapID"].split('/')[-1].lower()
+                side = lambda teamID: "Defending" if teamID == "Blue" else "Attacking"
+                print(f'Agent Select Found - {mapCodes[mapName].capitalize()} - ' + side(matchInfo['Teams'][0]['TeamID']) + ' first')
+
+                if (maps[mapName] != None):
+                    client.pregame_select_character(maps[mapName])
+                    client.pregame_lock_character(maps[mapName])
+                    print('Agent Locked - ' + list(agents.keys())[list(agents.values()).index(maps[mapName])].capitalize())
+
+
+        except:
+            print('', end='') # Using pass caused weird behavior
+    
+def check_state():
+    try:
+        sessionState = client.fetch_presence(client.puuid)['sessionLoopState']
+        if (sessionState == "MENUS"):
+            lock_agent()
+    except:
+        print('', end='') # Using pass caused weird behavior
 
 with open('data.json', 'r') as f:
     data = json.load(f)
@@ -31,8 +71,8 @@ if (ranBefore == False or choice == 'c'):
     client.activate()
 
     
-    print("\nPlease Enter Preferred Agent For Each Map (e.g kayo)")
-    print("_________________________________________________________\n")
+    print("\nPlease Enter Preferred Agent For Each Map (type NONE to not instalock on a map)")
+    print("_"*80, end="\n")
 
     for map in maps.keys():
         valid = False
@@ -42,9 +82,11 @@ if (ranBefore == False or choice == 'c'):
                 if (preferredAgent in agents.keys()):
                     maps[map] = agents[preferredAgent]
                     valid = True
+                elif (preferredAgent == "none"):
+                    valid = True
                 else:
                     print("Invalid Agent")
-            except:
+            except Exception as e:
                 print("Input Error")
     
     with open('data.json', 'w') as f:
@@ -59,19 +101,4 @@ else:
 
 
 print("Waiting for Agent Select\n")
-while True:
-    try:
-        sessionState = client.fetch_presence(client.puuid)['sessionLoopState']
-        matchID = client.pregame_fetch_match()['ID']
-        if ((sessionState == "PREGAME") and (matchID not in seenMatches)):
-            matchInfo = client.pregame_fetch_match(matchID)
-            mapName = matchInfo["MapID"].split('/')[-1].lower()
-            print(f'Agent Select Found - {mapCodes[mapName].capitalize()}')
-
-            client.pregame_select_character(maps[mapName])
-            client.pregame_lock_character(maps[mapName])
-            seenMatches.append(client.pregame_fetch_match()['ID'])
-            print('Agent Locked - ' + list(agents.keys())[list(agents.values()).index(maps[mapName])].capitalize())
-
-    except Exception as e:
-        print('', end='') # Using pass caused weird behavior
+set_interval(check_state, 4)

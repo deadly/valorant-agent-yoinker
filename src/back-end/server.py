fe56f8daab48ded4d8ca024e40f22c61a915a421
Neefs <69609852+Neefs@users.noreply.github.com>
2023-03-14 17:53:18 -0400

    more template code

--- src/back-end/server.py
+++ src/back-end/server.py
@@ -1,7 +1,23 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from valclient.client import Client
 from player import Player
+import json
+
+with open("settings/data.json", "r") as f:
+    data:dict = json.loads(f.read())
+
+def update_config():
+    with open("settings/data.json", "r") as f:
+        data = json.loads(f.read())
+
+
+
+def save_config():
+    with open("settings/data.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+
 
 # creates client and player object
 client = Client(region='na')
@@ -16,3 +32,18 @@ server = Flask(__name__, static_folder=guiDir, template_folder=guiDir)
 @server.route("/")
 def home():
     return render_template('index.html', name=player.name)
+
+print(data)
+
+@server.route("/config", methods=["GET", "POST"])
+def config():
+    keys = [i for i in data.keys()]
+    values = [i for i in data.values()]
+    if request.method == "POST":
+        
+        print(request.form.get())
+    
+    return render_template("config.html",
+                           keys=keys,
+                           values=values)
+

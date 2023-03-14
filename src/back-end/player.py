9aff336c5bf648e368d48148535745027e3a71b9
deadly <seraphownstheworld25@protonmail.ch>
2023-03-14 13:22:16 -0400

    added match logic, added method to find current team

--- src/back-end/player.py
+++ src/back-end/player.py
@@ -4,6 +4,8 @@ class Player:
         self.client = client
         self.puuid = self.client.puuid
         self.name = self.set_name(self.puuid)
+        self.seenMatches = [] # array of match IDs
+        self.currentMatch = None # contains ALL information about current match
 
     def set_name(self, puuid):
         playerData = self.client.put(
@@ -19,3 +21,18 @@ class Player:
     
     def lock_agent(self, agentID):
         self.client.pregame_lock_character(agentID)
+    
+    def acknowledge_current_match(self):
+        # append current match ID to seenMatches
+        self.seenMatches.append(self.client.pregame_fetch_match()['ID'])
+        # update currentMatch to the current pregame
+        self.currentMatch = self.client.pregame_fetch_match()
+    
+    def get_side(self):
+        teamID = self.currentMatch['Teams'][0]['TeamID']
+
+        if (teamID == 'Blue'):
+            return 'Defending'
+        else:
+            return 'Attacking'
+

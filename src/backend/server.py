76e0484e61026863d4008bf7d6e3073bff6b2746
deadly <seraphownstheworld25@protonmail.ch>
2023-03-15 10:44:29 -0400

    set up code to connect to websocket and run in separate thread

--- src/backend/server.py
+++ src/backend/server.py
@@ -3,17 +3,19 @@ from flask import Flask, render_template
 from valclient.client import Client
 from backend.player import Player
 
+
 # creates client and player object
 client = Client(region='na')
 client.activate()
 player = Player(client=client)
 
+
 # path for files for front-end
 guiDir = os.path.join(os.path.dirname(__file__), '..', 'frontend')
 
 if getattr(sys, 'frozen', False):
+    # update the frontend path accordingly if running the compiled version
     guiDir = os.path.join(sys._MEIPASS, 'src', 'frontend')
-    print(sys._MEIPASS)
     server = Flask(__name__, static_folder=guiDir, template_folder=guiDir)
 else:
     server = Flask(__name__, static_folder=guiDir, template_folder=guiDir)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import webview
 from backend.server import server
+from backend.websocket import startWs
 
-webview.create_window('Agent Yoinker', server)
-webview.start()
+window = webview.create_window('Agent Yoinker', server)
+webview.start(startWs) # start the window along with startWs in a separate thread
